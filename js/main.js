5093dcc49baa4a4ddc25561f73312162103d316e
tom-gordon13 <tombcgordon@gmail.com>
2022-04-01 15:41:41 -0600

    Break squares DOM into nested arrays, add assignMines function for visual testing purposes

--- js/main.js
+++ js/main.js
@@ -11,6 +11,9 @@ let boardArr;
 /*----- cached element references -----*/
 //grab board, will be used to create squares
 let boardDOM = document.getElementById('board');
+let squaresDOM; //cached later, after buttons are generated
+let squaresDOMNest;
+
 
 
 /*----- event listeners -----*/
@@ -21,38 +24,62 @@ let boardDOM = document.getElementById('board');
 // TEST FUNCTIONS/VARIABLES
 let size = 10
 let num = 10;
-initSquares(size);
-initBoard(size);
-initMines(num)
+init(10);
 
 // INITIALIZATION FUNCTIONS //
-function init() {
+function init(size) {
+    initSquares(size);
+    chunkSquares(size);
+    initBoard(size);
+    initMines(num);
+    initMarkers();
+}
 
+function initMarkers() {
+    markerArr = [];
+}
+
+function chunkSquares(size) {
+    // Turns squares DOM elements (buttons) into a nested array the same size as the board
+    squaresDOMNest = [];
+    squaresDOM = [].concat(...squaresDOM)
+
+    while (squaresDOM.length) {
+        squaresDOMNest.push(
+            squaresDOM.splice(0, size)
+        )
+    }
+
+    // return squaresDOMNest;
 }
 
 function initBoard(size) {
     //populate board data structure based on board size
     boardArr = new Array(size);
-    boardArr.fill(new Array(size))
+    boardArr.fill(new Array(size));
 }
 
-function initMines(num) {
+function initMines(numMines) {
+    // console.log(numMines);
     mineArr = [];
-    while (num > 0) {
-        let arr1 = Math.floor(Math.random() * num)
-        console.log(arr1)
-        let arr2 = Math.floor(Math.random() * num)
-        console.log(arr2)
-        mineArr.push({ name: `m${num}`, arr1: `[${arr1}]`, arr2: `[${arr2}]` });
-        num--;
+    while (numMines > 0) {
+        let arr1 = Math.floor(Math.random() * numMines);
+        let arr2 = Math.floor(Math.random() * numMines);
+        mineArr.push({ name: `m${numMines}`, arr1: `${arr1}`, arr2: `${arr2}` });
+        numMines = numMines - 1;
+
     }
+}
+
+function checkVicinity() {
+
 }
 
 function initSquares(size) {
 
     //Set board container size to fit squares
-    boardDOM.style.height = `${size * sqSize}vh`
-    boardDOM.style.width = `${size * sqSize}vh`
+    boardDOM.style.height = `${size * sqSize}vmin`
+    boardDOM.style.width = `${size * sqSize}vmin`
 
 
 
@@ -63,23 +90,29 @@ function initSquares(size) {
     while (numSquares > 0) {
         let square = document.createElement('button');
         square.setAttribute('class', 'square');
-        square.style.height = `${sqSize}vh`;
-        square.style.width = `${sqSize}vh`;
+        square.style.height = `${sqSize}vmin`;
+        square.style.width = `${sqSize}vmin`;
         boardDOM.appendChild(square);
         numSquares--;
     }
-}
-
-function initMines(num) {
-
+    squaresDOM = [...document.querySelectorAll('button')];
 }
 
 
-
-
+function assignMines() {
+    mineArr.forEach(function (mine) {
+        // console.log(mine.arr1, mine.arr2)
+        squaresDOMNest[mine.arr1][mine.arr2].id = 'mine';
+    })
+}
 
 
 // RENDER FUNCTIONS //
 function render() {
 
 }
+
+function renderMines() {
+
+}
+
