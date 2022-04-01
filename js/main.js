cadb588aca58516ca8c616454fd56f22add5f072
tom-gordon13 <tombcgordon@gmail.com>
2022-03-31 22:19:26 -0600

    Add logic to generate mine array, work on logic to init board size and squares

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 /*----- constants -----*/
-
-
+const sqSize = 4;
+const maxSquares = 20;
 
 /*----- app's state (variables) -----*/
-
+let mineArr = [];
+let markerArr;
+let boardArr;
 
 
 /*----- cached element references -----*/
@@ -16,6 +18,11 @@ let boardDOM = document.getElementById('board');
 
 
 /*----- functions -----*/
+// TEST FUNCTIONS/VARIABLES
+initSquares(10);
+initBoard(10);
+let size = 10
+let num = 10;
 
 // INITIALIZATION FUNCTIONS //
 function init() {
@@ -23,20 +30,53 @@ function init() {
 }
 
 function initBoard(size) {
-    if (size > 10) return;
-    let numSquares = size ** 2;
-    console.log(numSquares)
-    while (numSquares > 0) {
-        let square = document.createElement('div');
-        square.setAttribute('class', 'square');
-        square.style.height = `${boardDOM.clientHeight / size}px`
-        square.style.width = `${boardDOM.clientWidth / size}px`
-        boardDOM.appendChild(square);
-        numSquares--;
-    };
+    //populate board data structure based on board size
+    boardArr = new Array(size);
+    boardArr.forEach(function (element, idx) {
+        element = [];
+    })
 }
 
-initBoard(9);
+function initMines(num) {
+    while (num > 0) {
+        let arr1 = Math.floor(Math.random() * num)
+        console.log(arr1)
+        let arr2 = Math.floor(Math.random() * num)
+        console.log(arr2)
+        mineArr.push({ name: `m${num}`, arr1: `[${arr1}]`, arr2: `[${arr2}]` });
+        num--;
+    }
+}
+
+function initSquares(size) {
+
+    //Set board container size to fit squares
+    boardDOM.style.height = `${size * sqSize}vh`
+    boardDOM.style.width = `${size * sqSize}vh`
+
+
+
+    if (size > maxSquares) return;
+    let numSquares = size ** 2;
+
+    // Initialize new squares based on chosen board size
+    while (numSquares > 0) {
+        let square = document.createElement('button');
+        square.setAttribute('class', 'square');
+        square.style.height = `${sqSize}vh`;
+        square.style.width = `${sqSize}vh`;
+        boardDOM.appendChild(square);
+        numSquares--;
+    }
+}
+
+function initMines(num) {
+
+}
+
+
+
+
 
 
 // RENDER FUNCTIONS //
